33db3509705f5d1f4490de75589af85efb079cea
Jacob Simon <jacob.anthony.simon@gmail.com>
2016-10-19 00:54:22 -0700

    Update package.json, add port env var

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const server = require('http').Server(app);
 const ChatServer = require("react-chat-server");
 const chat = new ChatServer(server);
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(chat.expressMiddleware());
 
 app.use(express.static(__dirname + '/public'));
@@ -13,6 +15,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, () => {
-  console.log('Chat server up and running at localhost:3000');
+server.listen(app.get('port'), () => {
+  console.log('Chat server up and running at localhost:' + app.get('port'));
 });
